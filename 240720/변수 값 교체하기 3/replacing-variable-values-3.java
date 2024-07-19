e2ba27639bf4fbb93905a9b5606b5adbcbfcaa9c
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-20 00:09:35 +0900

    Title: 변수 값 교체하기 3, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240720/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int temp;
+        int a = 3;
+        int b = 5;
+        temp = b; // 5
+        b = a; // 3
+        a = temp; // 5
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
