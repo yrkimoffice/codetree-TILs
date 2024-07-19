eff7ce9e12ac7adb350b4a00396769d29df8f31a
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-20 00:51:56 +0900

    Title: 데이터 교환, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 240720/데이터 교환/exchange-data.java
+++ 240720/데이터 교환/exchange-data.java
@@ -2,12 +2,10 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 5, b = 6, c = 7;
-        int temp1, temp2; 
-        temp1 = b; // 6
-        b = a; // 5
-        temp2 = c; // 7
-        c = temp1; //6
-        a = temp2; // 7
+        int temp = a;
+        a = c; // 7
+        c = b; // 6
+        b = temp; // 5
         
         System.out.println(a);
         System.out.println(b);
