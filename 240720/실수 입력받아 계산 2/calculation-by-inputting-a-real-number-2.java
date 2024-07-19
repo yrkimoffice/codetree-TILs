a960b2e734e5532efdcdb3b53b7b69c44815d3e3
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-20 01:11:43 +0900

    Title: 실수 입력받아 계산 2, Time: 147ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240720/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double result = a + 1.5;
+        System.out.printf("%.2f", result);
+    }
+}
