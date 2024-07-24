a56284f2d00f506c5641651c79a82ca82d61fd03
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-24 22:43:07 +0900

    Title: 시험 통과 여부 확인하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240724/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 80) {
+            System.out.println("pass");
+        } else {
+            System.out.printf("%d more score", 80 - n);
+        }
+    }
+}
