88fd0d407e5280d7634126bcedbf08a8366600e5
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-25 23:51:00 +0900

    Title: 두 번의 연산, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240725/두 번의 연산/two-operations.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a >=1 && a <= 100) {
+            if (a % 2 == 1) {
+                a += 3;
+            }
+            if (a % 3 == 0) {
+                a = a / 3;
+            }
+
+            System.out.println(a);
+        }
+    }
+}
