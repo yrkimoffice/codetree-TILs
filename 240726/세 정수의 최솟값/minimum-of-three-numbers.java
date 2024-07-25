6fd2d937d9915004a6d2cc8722d20100fcf7fa9b
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-26 01:09:45 +0900

    Title: 세 정수의 최솟값, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // a, b, c
+        // b, a, c
+        // c, a, b
+
+        if (a <= b && a <= c) {
+            System.out.println(a);
+        } else if (b <= a && b <= c) {
+            System.out.println(b);
+        } else if (c <= a && c <= b) {
+            System.out.println(c);
+        }
+    }
+}
