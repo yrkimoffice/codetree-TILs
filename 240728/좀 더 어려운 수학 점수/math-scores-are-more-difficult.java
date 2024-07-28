49b54a3a4e99be52a7030cec9f33a5405485df14
yrkimoffice <63059169+yrkimoffice@users.noreply.github.com>
2024-07-28 23:08:33 +0900

    Title: 좀 더 어려운 수학 점수, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240728/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mathA = sc.nextInt();
+        int engA = sc.nextInt();
+
+        int mathB = sc.nextInt();
+        int engB =sc.nextInt();
+
+        // 수학점수가 높은 경우 더 높은 학생 이름 출력
+        // 수학점수가 같으면 영어점수가 더 높은 학생 이름 출력
+        System.out.println(mathA > mathB ? "A" : "B");
+        if (mathA == mathB) {
+            System.out.println(engA > engB ? "A" : "B");
+        }
+    }
+}
